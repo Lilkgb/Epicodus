44aab0d420a96f1cdfcab43b18b31b8961b8af65
Joe Pritchett and Ryan McLean <joe@>
2019-02-21 09:35:53 -0800

    took off borders. changed search icon for mobile. fixed background dark theme. change light theme font color to white

--- js/scripts.js
+++ js/scripts.js
@@ -4,12 +4,20 @@ $(document).ready(function() {
     $("body").addClass("dark")
     $("#darkTheme").hide();
     $("#lightTheme").show();
+    $("#lightLogo").hide();
+    $("#darkLogo").show();
+    $("#lightSearch").hide();
+    $("#darkSearch").show();
   });
   $("button#lightTheme").click(function() {
     $("body").removeClass();
     $("body").addClass("light");
     $("#lightTheme").hide();
     $("#darkTheme").show();
+    $("#lightLogo").show();
+    $("#darkLogo").hide();
+    $("#lightSearch").show();
+    $("#darkSearch").hide();
   });
   $(".box").click(function() {
     $(this).find(".dropDown").slideToggle("slow");
